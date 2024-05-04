381beb756d39fcaf24df9ab5991847025fe870f1
William Su <williamsu@Williams-MacBook-Pro.local>
2024-05-04 14:17:56 +0800

    can sort by name address transaction

--- main.go
+++ main.go
@@ -3,13 +3,14 @@ package main
 import (
 	"encoding/json"
 	"fmt"
-	"sort"
 	"io/ioutil"
+	"sort"
 )
 
 type ContractType string
 type Status string
 type Order string
+type SortBy string
 
 const (
 	Ok    Status = "ok"
@@ -22,23 +23,24 @@ const (
 )
 
 const (
-	Ascend Order = "ascend"
-	Descend  Order = "descend"
+	Name        SortBy = "name"
+	Address     SortBy = "address"
+	Transaction SortBy = "transaction"
 )
 
 type JsonPayload struct {
-	SortBy string 
-	Order Order
-	Contracts []Contract 
+	SortBy     string
+	Descending bool `json:",string"`
+	Contracts  []Contract
 }
 
 type Contract struct {
-	Name         string 
-	Address      string 
-	Transaction  string 
+	Name         string
+	Address      string
+	Transaction  string
 	Block        int `json:",string"`
-	ContractType ContractType 
-	Status       Status 
+	ContractType ContractType
+	Status       Status
 }
 
 func main() {
@@ -58,27 +60,53 @@ func main() {
 	}
 
 	fmt.Println("Sort by:", data.SortBy)
-	fmt.Println("Sort by:", data.Order)
+	fmt.Println("Sort by:", data.Descending)
 	fmt.Println("Contracts:", data.Contracts)
 
-	// sortByName(data.Contracts)
-	
-	// fmt.Println("Contracts:", data.Contracts)
+	var myMap = map[string]func([]Contract, bool){
+		"name":        sortByName,
+		"address":     sortByAddress,
+		"transaction": sortByTransaction,
+	}
 
-	sortByAddress(data.Contracts)
+	sortFunc := myMap[data.SortBy]
+	sortFunc(data.Contracts, data.Descending)
+	fmt.Println(data.SortBy, data.Descending)
 	fmt.Println("Contracts:", data.Contracts)
-
-}	
-
-func sortByName (contracts []Contract) {
-	sort.Slice(contracts[:], func(i, j int) bool {
-		return contracts[i].Name < contracts[j].Name
-	})
 }
 
+func sortByName(contracts []Contract, descending bool) {
+	if descending {
+		sort.Slice(contracts[:], func(i, j int) bool {
+			return contracts[i].Name > contracts[j].Name
+		})
+	} else {
+		sort.Slice(contracts[:], func(i, j int) bool {
+			return contracts[i].Name < contracts[j].Name
+		})
+	}
+}
 
-func sortByAddress (contracts []Contract) {
-	sort.Slice(contracts[:], func(i, j int) bool {
-		return contracts[i].Address < contracts[j].Address
-	})
+func sortByAddress(contracts []Contract, descending bool) {
+	if descending {
+		sort.Slice(contracts[:], func(i, j int) bool {
+			return contracts[i].Address > contracts[j].Address
+		})
+	} else {
+		sort.Slice(contracts[:], func(i, j int) bool {
+			return contracts[i].Address < contracts[j].Address
+		})
+	}
+}
+
+func sortByTransaction(contracts []Contract, descending bool) {
+	if descending {
+		sort.Slice(contracts[:], func(i, j int) bool {
+			return contracts[i].Transaction > contracts[j].Transaction
+		})
+	} else {
+		sort.Slice(contracts[:], func(i, j int) bool {
+			return contracts[i].Transaction < contracts[j].Transaction
+		})
+	}
 }
